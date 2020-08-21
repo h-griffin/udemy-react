67d97b235c72f9360f03149943758b0f0b6bdcf5
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-08-21 13:09:00 -0700

    person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>hello, world!</h1>
+                <p>small stuff</p>
+                <Person/>
             </div>
         );
 
